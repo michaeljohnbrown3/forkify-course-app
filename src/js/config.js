e695b3bc533a8ea8dab80331735f330d7289d98b
michaeljohnbrown3 <michaeljohnbrown3@gmail.com>
2022-08-01 18:05:44 -0400

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
+export const KEY = '0dee67be-5d21-4784-ba68-8f11df7176c0';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const MODAL_CLOSE_SEC = 2.5;
